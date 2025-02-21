3eee30dc13ac8cac50a0e42e754b771d6f6d793d
Edrick Lesigues <elesigue@csuchico.edu>
2025-02-20 18:00:26 -0800

    Need to finish log in and sign up

---
+++ bets/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home, user_login, user_logout
+
+urlpatterns = [
+    path('', home, name='home'),  # Homepage URL
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+]
--- bets/views.py
+++ bets/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1>Welcome to the Sports Betting Site!</h1>")
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")  # Redirect to homepage after login
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, "login.html", {"form": form})
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
